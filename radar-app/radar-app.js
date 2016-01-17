166ea7d46b9108938133d254327239b44b8b515d
Fernando Iodice <feriodice@gmail.com>
2016-01-17 15:35:00 -0200

    New html template

--- radar-app/radar-app.js
+++ radar-app/radar-app.js
@@ -49,56 +49,7 @@ if (Meteor.isClient) {
     map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
     var infoWindow = new google.maps.InfoWindow(), marker, i;
-    console.log("this is running");
-
-
-    // HTTP.get(Meteor.absoluteUrl("/data/stops.txt"), function(err,result) {
-    //     console.log("i am in the isClient");
-    //     console.log(result.content);
-    //     var stopsData = result.content;
-    //     var parsedStopsData = CSVToArray(stopsData, ",");
-    //     console.log(parsedStopsData);
-    //     parsedStopsData.shift();
-        
-
-    // });
-
-
-
-//shapes
-/*
-    HTTP.get(Meteor.absoluteUrl("/data/shapes.txt"), function(err,result) {
-       // console.log(result.content);
-        var path_Data = result.content;
-        console.log(path_Data[3]);
-        var parsedpath_Data = CSVToArray(path_Data, ",");
-        parsedpath_Data.shift();
-        //markers = parsedStopsData;
-        var prev = 0;
-        var busline = [];
-        var road = '';
-        for (i in parsedpath_Data) {
-          if (parsedpath_Data[prev][0] != parsedpath_Data[i][0]){ 
-            var color = '#'+Math.floor(Math.random()*16777215).toString(16);
-            prev=i;
-            busPath = new google.maps.Polyline({
-              path: busline,
-              geodesic: true,
-              strokeColor: color,
-              strokeOpacity: 1.0,
-              strokeWeight: 2
-              });
-            busline=[];
-            road=' ';
-            busline.push({lat: parseFloat(parsedpath_Data[i][1]), lng: parseFloat(parsedpath_Data[i][2])});
-            busPath.setMap(map);
-          }
-          else{
-            busline.push({lat: parseFloat(parsedpath_Data[i][1]), lng: parseFloat(parsedpath_Data[i][2])});
-          }
-        };
-    });*/
-
+    console.log("larissa run");
 
 //heatmap
 HTTP.get(Meteor.absoluteUrl("/data/stops.txt"), function(err,result) {
@@ -114,180 +65,52 @@ HTTP.get(Meteor.absoluteUrl("/data/stops.txt"), function(err,result) {
          }
   return points};
 
-         /*for (i in parsedpath_Data){
-          var next = new google.maps.LatLng(parseFloat(parsedpath_Data[i][1]), parseFloat(parsedpath_Data[i][2]));
-          console.log(next);
-          points.push(next);
-         }
-        */ 
         heatmap = new google.maps.visualization.HeatmapLayer({
     data: getPoints(parsedpath_Data),
     map: map
   });
       });
-
-
-//parsing addresses
-/*
-  HTTP.get(Meteor.absoluteUrl("/data/addresses.csv"), function(err,result) {
-         // console.log(result.content);
-          var addr_Data = result.content;
-          var parsedaddr_Data = CSVToArray(addr_Data, ",");
-          console.log(parsedaddr_Data[30]);
-          console.log(addressTOstring(parsedaddr_Data[30]));
-          /*
-          HTTP.call('GET','https://maps.googleapis.com/maps/api/geocode/json?address=' + addressTOstring(parsedaddr_Data[30])+'&key=AIzaSyA_2Qi3MVVByu9nwkBPNt2hYUn7SHooP10',{},function(err,result){
-                console.log(result.content);
-                  });
-  });
-  HTTP.get(Meteor.absoluteUrl("/data/addresses2.csv"), function(err,result) {
-         // console.log(result.content);
-          var addr_Data = result.content;
-          var parsedaddr_Data = CSVToArray(addr_Data, ",");
-          console.log(parsedaddr_Data[38]);
-          console.log(address2TOstring(parsedaddr_Data[38]));
-          /*
-          HTTP.call('GET','https://maps.googleapis.com/maps/api/geocode/json?address=' + address2TOstring(parsedaddr_Data[38])+'&key=AIzaSyA_2Qi3MVVByu9nwkBPNt2hYUn7SHooP10',{},function(err,result){
-                console.log(result.content);
-                  });
-
-  });
-*/
-//addresses3
-
-  HTTP.get(Meteor.absoluteUrl("/data/all_addresses.csv"), function(err,result) {
-         // console.log(result.content);
-         l4=['0136', '1200', '1540', '2489', '2486', '2487', '0424', '2328', '2483', '0636', '2402', '2403', '2400', '2401', '2014', '0304', '0124', '1202', '1312', '0121', '0120', '2490', '0840', '1704', '0724', '2491', '1824', '2493', '1624', '4400', '2411', '2410', '2413', '2412', '2415', '2414', '2417', '2416', '2419', '1411', '0336', '0112', '0113', '0110', '0924', '0116', '0117', '1324', '0115', '1120', '0052', '1328', '0119', '1124', '0109', '4800', '4801', '4803', '1716', '0524', '0916', '1856', '0756', '0208', '1404', '2443', '2440', '1336', '2446', '0108', '1232', '0105', '0104', '0107', '0106', '0101', '0100', '2484', '2449', '2418', '0908', '2460', '0356', '0948', '2485', '0456', '1004', '0452', '2461', '1224', '2459', '2458', '2457', '1320', '0111', '2802', '2800', '2801', '2804', '2200', '0340', '2204', '1504', '0118', '2815', '2429', '3601', '0000', '2810', '0002', '2420', '2421', '2422', '2423', '2424', '2426', '2427', '1203', '2140', '3204', '3200', '0804', '0800', '0802', '1101', '2104', '2428', '2439', '2438', '0013', '3602', '1205', '2437', '2436', '2435', '2434', '0123', '0122', '1616', '4001', '4002', '0154', '1756', '0024', '0324', '0224', '2492', '5200', '2120', '0417', '2124', '1600', '1602', '0824', '2236', '2002', '2000', '2001', '0816', '2224'];
-          var addr_Data2 = result.content;
-          var parsedaddr_Data2 = CSVToArray(addr_Data2, ",");
-          parsedaddr_Data2.shift();
-          var icon = {
-          url: "../img/car.png", // url
-          scaledSize: new google.maps.Size(30, 30), // scaled size
-          origin: new google.maps.Point(0,0), // origin
-          anchor: new google.maps.Point(0, 0) // anchor
-        };
-          for( i = 1; i < 6; i++ ) {
-          //parseFloat(l4[i])
-          var index = parsedaddr_Data2.indexOf(l4[i]);
-          console.log(parsedaddr_Data2[index]);
-          console.log(address3TOstring(parsedaddr_Data2[index]));
-          HTTP.call('GET','https://maps.googleapis.com/maps/api/geocode/json?address=' + address3TOstring(parsedaddr_Data2[index])+'&key=AIzaSyA_2Qi3MVVByu9nwkBPNt2hYUn7SHooP10',{},function(err,result){
-              bigdata = JSON.parse(result.content);
-              //console.log(bigdata.results);
-              //console.log(bigdata.results[0].geometry.location.lat);
-              // console.log(result.content.location);
-              // console.log(result["location"]);
-              // console.log(result.content[0]);
-              var point = {lat: parseFloat(bigdata.results[0].geometry.location.lat), lng: parseFloat(bigdata.results[0].geometry.location.lng)};
-              newmarker = new google.maps.Marker({
-              position: point,
-              icon: icon,
-              map: map});
-          console.log(parsedaddr_Data2[parseFloat(l4[i])]);
-          console.log(address3TOstring(parsedaddr_Data2[parseFloat(l4[i])]));
-                  });
-          };
-
-  });
-
-/*
-//radars
-HTTP.get(Meteor.absoluteUrl("/data/radarl4.csv"), function(err,result) {
-        var radarData = result.content;
-        var parsedradarData = CSVToArray(radarData, ",");
-        var icon = {
-          url: "../img/car.png", // url
-          scaledSize: new google.maps.Size(30, 30), // scaled size
-          origin: new google.maps.Point(0,0), // origin
-          anchor: new google.maps.Point(0, 0) // anchor
-        };
-
-        console.log(parsedradarData.length);
-        for( i = 0; i < parsedradarData[0].length; i++ ) {
-              var point = {lat: parseFloat(parsedradarData[i][2]), lng: parseFloat(parsedradarData[i][3])};
-              newmarker = new google.maps.Marker({
-              position: point,
-              icon: icon,
-              map: map});
-          };
-
-    });
-*/
-
-
-
-
-
-  /*
-////1015-10-0 5:00:00 5:00:00 301703  R. EnÃ©as De Camargo, 36  1 -23.418553  -46.805319
-    HTTP.get(Meteor.absoluteUrl("/data/bus_path.csv"), function(err,result) {
-       // console.log(result.content);
-        var path_Data = result.content;
-        var parsedpath_Data = CSVToArray(path_Data, ",");
-        console.log(parsedpath_Data[0]);
-        parsedpath_Data.shift();
-        //markers = parsedStopsData;
-        var prev = 0;
-        var busline = [];
-        var road = '';
-        for (i in parsedpath_Data) {
-          if (parsedpath_Data[prev][0] != parsedpath_Data[i][0]){ 
-            console.log(road);
-            var color = '#'+Math.floor(Math.random()*16777215).toString(16);
-
-    /*        var result = HTTP.call('GET','https://roads.googleapis.com/v1/snapToRoads?path=' + road + '&interpolate=true&key=AIzaSyCMy95QAeFlYo1ZW4I52OhDVLIi3gDfPJg',{},function(err,result){
-                  return result;
-                    });
-            console.log(result);
-
-            prev=i;
-            busPath = new google.maps.Polyline({
-              path: busline,
-              geodesic: true,
-              strokeColor: color,
-              strokeOpacity: 1.0,
-              strokeWeight: 2
-              });
-            busline=[];
-            road=' ';
-            busline.push({lat: parseFloat(parsedpath_Data[i][6]), lng: parseFloat(parsedpath_Data[i][7])});
-            road = road.concat(parsedpath_Data[i][6]+',' + parsedpath_Data[i][7]);
-            busPath.setMap(map);
-          }
-          else{
-            busline.push({lat: parseFloat(parsedpath_Data[i][6]), lng: parseFloat(parsedpath_Data[i][7])});
-            road= road.concat(parsedpath_Data[i][6]+',' + parsedpath_Data[i][7])+'|';
-          }
-        };
-    });
-  */
-
-  }
 }
 
+  // HTTP.get(Meteor.absoluteUrl("/data/all_addresses.csv"), function(err,result) {
+  //        l4=['0136', '1200', '1540', '2489', '2486', '2487', '0424', '2328', '2483', '0636', '2402', '2403', '2400', '2401', '2014', '0304', '0124', '1202', '1312', '0121', '0120', '2490', '0840', '1704', '0724', '2491', '1824', '2493', '1624', '4400', '2411', '2410', '2413', '2412', '2415', '2414', '2417', '2416', '2419', '1411', '0336', '0112', '0113', '0110', '0924', '0116', '0117', '1324', '0115', '1120', '0052', '1328', '0119', '1124', '0109', '4800', '4801', '4803', '1716', '0524', '0916', '1856', '0756', '0208', '1404', '2443', '2440', '1336', '2446', '0108', '1232', '0105', '0104', '0107', '0106', '0101', '0100', '2484', '2449', '2418', '0908', '2460', '0356', '0948', '2485', '0456', '1004', '0452', '2461', '1224', '2459', '2458', '2457', '1320', '0111', '2802', '2800', '2801', '2804', '2200', '0340', '2204', '1504', '0118', '2815', '2429', '3601', '0000', '2810', '0002', '2420', '2421', '2422', '2423', '2424', '2426', '2427', '1203', '2140', '3204', '3200', '0804', '0800', '0802', '1101', '2104', '2428', '2439', '2438', '0013', '3602', '1205', '2437', '2436', '2435', '2434', '0123', '0122', '1616', '4001', '4002', '0154', '1756', '0024', '0324', '0224', '2492', '5200', '2120', '0417', '2124', '1600', '1602', '0824', '2236', '2002', '2000', '2001', '0816', '2224'];
+  //         var addr_Data2 = result.content;
+  //         var parsedaddr_Data2 = CSVToArray(addr_Data2, ",");
+  //         parsedaddr_Data2.shift();
+  //         var icon = {
+  //         url: "../img/car.png", // url
+  //         scaledSize: new google.maps.Size(30, 30), // scaled size
+  //         origin: new google.maps.Point(0,0), // origin
+  //         anchor: new google.maps.Point(0, 0) // anchor
+  //       };
+  //         for( i = 1; i < 6; i++ ) {
 
+  //         var index = parsedaddr_Data2.indexOf(l4[i]);
+  //         console.log(parsedaddr_Data2[index]);
+  //         console.log(address3TOstring(parsedaddr_Data2[index]));
+  //         HTTP.call('GET','https://maps.googleapis.com/maps/api/geocode/json?address=' + address3TOstring(parsedaddr_Data2[index])+'&key=AIzaSyA_2Qi3MVVByu9nwkBPNt2hYUn7SHooP10',{},function(err,result){
+  //             bigdata = JSON.parse(result.content);
+  //             var point = {lat: parseFloat(bigdata.results[0].geometry.location.lat), lng: parseFloat(bigdata.results[0].geometry.location.lng)};
+  //             newmarker = new google.maps.Marker({
+  //             position: point,
+  //             icon: icon,
+  //             map: map});
+  //         console.log(parsedaddr_Data2[parseFloat(l4[i])]);
+  //         console.log(address3TOstring(parsedaddr_Data2[parseFloat(l4[i])]));
+  //                 });
+  //         };
 
-// while (stops.hasNext()) {
-//    print(tojson(stops.next()));
-// }
-// Stops.each(function(err, item){
-//   console.log(item);
-//   console.log("this is an item");
-// });
+  // });
+}
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
     console.log("this is running");
-
-
   });
 }
 
 function CSVToArray( strData, strDelimiter ){
-    // Check to see if the delimiter is defined. If not,
-    // then default to comma.
     strDelimiter = (strDelimiter || ",");
 
     // Create a regular expression to parse the CSV values.
